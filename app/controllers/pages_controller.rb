f99f5b5e33219cf57288560a58bb5247cc5d9d9f
pkorenev <p.korenev@voroninstudio.eu>
2017-10-04 13:07:32 +0300

    app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     set_page_metadata(:home)
     @main_banner_article = @articles.find(2)
     @main_banner_articles = Article.limit(4)
+    @issue_date = @articles.first.release_date
   end
 
 end
